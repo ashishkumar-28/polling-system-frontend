c9852ccb89b8cce971f3369870387c1f17bcb047
Ashish <ak2831637@gmail.com>
2024-09-10 14:36:30 +0530

    Updated frontend for Vercel deployment

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:5000');
+const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000');
 export default socket;
+
